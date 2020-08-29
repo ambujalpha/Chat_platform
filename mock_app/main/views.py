01e6c7a8a191a5198f44e0c8c05584b249d5d564
ambujalpha <jainambuj8@gmail.com>
2020-08-29 18:20:27 +0530

    urls_fixes

--- mock_app/main/views.py
+++ mock_app/main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def index(request):
     return render(request,'front/index.html')
+
+
+def room(request, room_name):
+    return render(request, 'front/room.html', {
+        'room_name': room_name
+    })
