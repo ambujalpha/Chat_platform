e77040427fdce671353b344a3aec17847c2a0a56
ambujalpha <jainambuj8@gmail.com>
2020-08-29 20:35:45 +0530

    readme_complete

--- mock_app/mock_app/routing.py
+++ mock_app/mock_app/routing.py
@@ -3,7 +3,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import main.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
             main.routing.websocket_urlpatterns
